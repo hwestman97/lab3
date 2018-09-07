05fde870f996c1a1b09207ebb9ec1af566bf27d9
hwestman97 <36103995+hwestman97@users.noreply.github.com>
2018-09-07 11:49:36 +0200

    Add files via upload
    I stort sett bara döpt tomma funktioner, börjat fundera lite på hur man läser data ur filen

---
+++ lab3.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  6 13:20:35 2018
+
+@author: Hanna
+"""
+
+def uppgift_a():
+    pass
+
+def uppgift_b():
+    pass
+
+def uppgift_c():
+    pass
+
+def main():
+    '''
+    Prompts the user to choose if they whish to solve the problem using pure
+    python, A, numbpy, B, or numbpy and scipy, C.
+    '''
+    pass
+
+file = input('Vad är namnet på din fil? ')
+text = open(file, 'r')
+n = 0
+for line in text:
+    n += 1
+print(n)
