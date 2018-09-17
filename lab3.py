5a6cae9de3faaf299dd9bd30c664bf66bf136c8b
Hanna <ha.westman@gmail.com>
2018-09-17 21:23:51 +0100

    Omorganiserat och tagit bort kod-dubletter

--- lab3.py
+++ lab3.py
@@ -9,37 +9,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 
-def a_b_c(text):
+
+def a_b_c():
     '''
-    Prompts the user to choose if they whish to solve the problem using pure
-    python, A, numbpy, B, or numbpy and scipy, C.
+    Frågar användaren om de vill lösa problemet med bara python, alternativ A, 
+    numbpy, alternativ B, eller numbpy och scipy, alternativ C.
     '''
     x = True
     while x == True:
         choice = input('Vill du använda metod a, b, eller c? ')
         choice = choice.lower()
         if choice == 'a':
-            uppgift_a(text)
+            f, file= uppgift_a()
+            save_file(choice, file, f)
             x = False
         elif choice == 'b':
-            uppgift_b(text)
+            f, file = uppgift_b()
+            save_file(choice, file, f)
             x = False
         elif choice == 'c':
-            uppgift_c(text)
+            f, file = uppgift_c()
+            save_file(choice, file, f)
             x = False
         else:
             print('Datorn förstod inte vad du ville göra, försök igen!')
 
-def uppgift_a(text):
-    dictionary_xy = x_y(text)
-    alpha, beta = calc(dictionary_xy)
+def uppgift_a():
+    text, file = open_file()
+    dictionary_xy, alpha, beta = calc(text)
+    print_alphabeta(alpha, beta)
     f = plotGraph(dictionary_xy, alpha, beta)
-    return f
+    return f, file
+
+def save_file(choice, file, f):
+    f.savefig('%s_%s.pdf' % (file, choice))
+
+def print_alphabeta(alpha, beta):
+    print(u'\u03B1'+ ' = '+ str(alpha))
+    print(u'\u03B2'+ ' = '+ str(beta))
 
 def plotGraph(dictionary, alpha, beta):
     f = plt.figure()
     for key in dictionary:
-        plt.scatter(key, dictionary[key], c='darkblue')
+        plt.scatter(key, dictionary[key], c='blue')
     plt.xlabel('x')
     plt.ylabel('y')
     x1 = min(dictionary)
@@ -48,27 +60,29 @@ def plotGraph(dictionary, alpha, beta):
     y2 = alpha + beta*x2
     plt.plot([x1,x2],[y1,y2],linewidth=3.0, c='red')
     plt.show()
-    return f
+    return f    
 
-def uppgift_b(text):
+def uppgift_b():
+    text, file = open_file()
     x, y = b_c(text)
     beta, alpha = np.polyfit(x, y, 1)
-    print(u'\u03B1'+ ' = '+ str(alpha))
-    print(u'\u03B2'+ ' = '+ str(beta))
+    print_alphabeta(alpha, beta)
+    f = plt.figure()
     plt.scatter(x,y)
-    plt.plot(x, beta * x + alpha, '-', color='red')
+    plt.plot(x, beta * x + alpha, '-',linewidth=3.0, color='red')
     plt.show()
+    return f, file
 
-def uppgift_c(text):
-    a = np.loadtxt(text)
-    x = a[0:,0]
-    y = a[0:,1]
+def uppgift_c():
+    text, file = open_file()
+    x, y = b_c(text)
     beta, alpha, r_value, p_value, std_err = stats.linregress(x, y)
-    print(u'\u03B1'+ ' = '+ str(alpha))
-    print(u'\u03B2'+ ' = '+ str(beta))
+    print_alphabeta(alpha, beta)
+    f = plt.figure()
     plt.scatter(x,y)
-    plt.plot(x, beta * x + alpha, '-', color='red')
+    plt.plot(x, beta * x + alpha, '-',linewidth=3.0, color='red')
     plt.show()
+    return f, file
 
 def b_c(text):
     a = np.loadtxt(text)
@@ -82,17 +96,14 @@ def open_file():
     file = file[:-4]
     return text, file
 
-def x_y(text):
+def calc(text):
     xy = {}
     for line in text:
-        no_new_line = line.strip()
+        no_new_line = line.strip('\n')
         new_line = no_new_line.split("\t")
         x_i = float(new_line[0])
         y_i = float(new_line[1])
         xy[x_i] = y_i
-    return xy
-
-def calc(xy):
     m = len(xy)
     sum_x = 0
     sum_y = 0
@@ -107,15 +118,10 @@ def calc(xy):
         square_y += xy[key]**2
     beta = (m*product_xy - sum_x*sum_y)/(m*square_x - sum_x*sum_x)
     alpha = sum_y/m - beta*sum_x/m
-    print(u'\u03B1'+ ' = '+ str(alpha))
-    print(u'\u03B2'+ ' = '+ str(beta))
-    return alpha, beta
+    return xy, alpha, beta
 
 def main():
-    text, file = open_file()
-    #f = 
-    a_b_c(text)
-    #f.savefig('%s_a.pdf' % file)
+    a_b_c()
     
 main()
 '''
@@ -123,4 +129,6 @@ open_file ska inte köras i b & c, eller iaf inte i c
 rubriker och skit i graferna
 slå ihop plotbestämningarna för a, b, c
 skriv om open_file???
+Astrids grej om random
+gör dig av med listor
 '''
