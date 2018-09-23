5183f301e6334800f6496fe62ee68d9fcba1f1ee
hwestman97 <36103995+hwestman97@users.noreply.github.com>
2018-09-23 17:21:04 +0200

    Gjort allting sämre

--- lab3.py
+++ lab3.py
@@ -9,70 +9,64 @@ font = {'family': 'serif',
         'size': 20,
         }
 
+errors = (ValueError, FileNotFoundError, ZeroDivisionError, IOError, IndexError, OSError)
+
+#font och errors är globala variabler och jag har valt att ha det så
+#för att de används i flera olika funktioner utan att man ändrar deras värden
+
 def main():
     '''
     Frågar användaren om de vill lösa problemet med bara python, alternativ A, 
-    numbpy, alternativ B, eller numbpy och scipy, alternativ C.
+    numbpy, alternativ B, eller numbpy och scipy, alternativ C, skriv sedan in 
+    en giltig fil.
     '''
+    
     print('''Det här programmet tar en fil med x- och y-värden och utför en enkel 
 linjär regression på punkterna. Programmet retunerar värdena på alfa och beta 
 och plottar sedan en graf med både punkterna och regressionslinjen som sparas
 som en pdf. Metod a använder sig av ren python, metod b av numpy och metod c 
 av både numpy och scipy. Avsluta programmet genom att trycka Q.''')
+    
     Loop = True 
     while Loop == True:
         choice = input('Vill du använda metod a, b, eller c? ')
         choice = choice.lower() #Omvandlar alla bokstäver till små bokstäver
-        if choice == 'a' or choice == 'b' or choice == 'c':
-            get_file(choice)
+        if choice == 'q':
             Loop = False
-        elif choice == 'q':
+        elif choice == 'a':
+            file, filename = get_file()
+            uppgift_a(file, choice, filename)
+            Loop = False
+        elif choice == 'b':
+            file, filename = get_file()
+            uppgift_b(file, choice, filename)
+            Loop = False
+        elif choice == 'c':
+            file, filename = get_file()
+            uppgift_c(file, choice, filename)
             Loop = False
         else:
             print('Datorn förstod inte vad du ville göra, försök igen!')
 
-def get_file(choice):
-    '''
-    Skriv in ett giltigt filnamn med x- och y-värden, funktionen kollar att filen
-    finns och inte är tom, anropar sedan funktionen som korresponderar till den 
-    metod du valde. Till sist sparas grafen i pdf-format.
-    '''
-    Loop = True
-    felmeddelande = '\nNågot är fel med din fil, försök igen!'
-    error = (ValueError, FileNotFoundError, ZeroDivisionError, IOError, IndexError)
-    #Loopen är till för att användaren ska få försöka skriva in en fil flera gånger
-    #Funktionen testar också för vanliga fel med filen
-    while Loop == True:
-        file = input('Vad är namnet på din fil? ')
-        filename = file[:-4]
-        try:
-            if file == 'q':
-                Loop = False
-            elif choice == 'a':
-                    f = uppgift_a(file)
-                    save_file(choice, filename, f)
-                    Loop = False
-            elif choice == 'b':
-                    f = uppgift_b(file)
-                    save_file(choice, filename, f)
-                    Loop = False
-            elif choice == 'c':
-                    f = uppgift_c(file)
-                    save_file(choice, filename, f)
-                    Loop = False
-        except error:
-            print(felmeddelande)
+def get_file():
+    file = input('Vad är namnet på din fil? ')
+    filename = file[:-4]
+    return file, filename
 
-def uppgift_a(file):
+def uppgift_a(file, choice, filename):
     '''
     Det här är metod a:s kodskelett, funktionen kallar på de nödvändiga subfunktionerna
     för att kunna gå från en fil med x- och y-värden till en graf med värden på
     alfa och beta.
     '''
-    dictionary_xy, alpha, beta = calc(file)
-    print_alphabeta(alpha, beta)
-    f = plot_a(dictionary_xy, alpha, beta, file)
-    return f
+    try:
+        dictionary_xy, alpha, beta = calc(file)
+        print_alphabeta(alpha, beta)
+        f = plot_ab(dictionary_xy, alpha, beta, file)
+        save_file(choice, filename, f)
+        return f
+    except errors:
+        print('\nNågot är fel med din fil!')
 
 def save_file(choice, file, f):
     '''
@@ -87,7 +81,7 @@ def print_alphabeta(alpha, beta):
     print(u'\u03B1'+ ' = '+ str(alpha))
     print(u'\u03B2'+ ' = '+ str(beta))
 
-def plot_a(dictionary, alpha, beta, file):
+def plot_ab(dictionary, alpha, beta, file):
     '''
     Plottar grafen med metod a, dvs ren python.
     '''
@@ -107,31 +101,39 @@ def plot_a(dictionary, alpha, beta, file):
     plt.show()
     return f    
 
-def uppgift_b(file):
+def uppgift_b(file, choice, filename):
     '''
     Det här är metod b:s kodskelett, funktionen kallar på de nödvändiga subfunktionerna
     för att kunna gå från en fil med x- och y-värden till en graf med värden på
     alfa och beta.
-    '''    
-    x, y = b_c(file)
-    beta, alpha = np.polyfit(x, y, 1)
-    print_alphabeta(alpha, beta)
-    f = plot_bc(x, y, alpha, beta, file)
-    return f
+    '''
+    try:
+        dictionary_xy, alpha, beta = calc(file)
+        print_alphabeta(alpha, beta)
+        f = plot_ab(dictionary_xy, alpha, beta, file)
+        save_file(choice, filename, f)
+        return f
+    except errors:
+        print('\nNågot är fel med din fil!')
 
-def uppgift_c(file):
+def uppgift_c(file, choice, filename):
     '''
     Det här är metod c:s kodskelett, funktionen kallar på de nödvändiga subfunktionerna
     för att kunna gå från en fil med x- och y-värden till en graf med värden på
     alfa och beta.
-    '''    
-    x, y = b_c(file)
-    beta, alpha, r_value, p_value, std_err = stats.linregress(x, y)
-    print_alphabeta(alpha, beta)
-    f = plot_bc(x, y, alpha, beta, file)
-    return f
+    '''
+    try:
+        a = np.loadtxt(file)
+        x = a[:,0]
+        y = a[:,1]
+        beta, alpha, r_value, p_value, std_err = stats.linregress(x, y)
+        print_alphabeta(alpha, beta)
+        f = plot_c(x, y, alpha, beta, file)
+        save_file(choice, filename, f)
+    except errors:
+        print('\nNågot är fel med din fil!')
 
-def plot_bc(x, y, alpha, beta, file):
+def plot_c(x, y, alpha, beta, file):
     '''
     Funktionen plottar en graf enligt metod b och c.
     '''
@@ -145,15 +147,6 @@ def plot_bc(x, y, alpha, beta, file):
     plt.title(file, fontdict=font)
     plt.show()   
     return f
-
-def b_c(file):
-    '''
-    Omvandlar användarens fil till två arrays som sedan retuneras.
-    '''
-    a = np.loadtxt(file)
-    x = a[0:,0]
-    y = a[0:,1]
-    return x, y
 
 def calc(file):
     '''
